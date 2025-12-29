5ecd6e31248103bcb0504c6f2f2c5ceda534327a
Igor Bazhev <bazhew@mail.ru>
2025-12-29 17:41:59 +0500

    Change the names of the endpoints

--- src/main/java/io/github/onlineshop/constants/PathConstants.java
+++ src/main/java/io/github/onlineshop/constants/PathConstants.java
@@ -3,9 +3,9 @@ package io.github.onlineshop.constants;
 public class PathConstants {
     private static final String API_PATH = "/api";
 
-    public static final String USER = API_PATH + "/user";
-    public static final String ORDER = API_PATH + "/order";
-    public static final String PRODUCT = API_PATH + "/product";
+    public static final String USER = API_PATH + "/users";
+    public static final String ORDER = API_PATH + "/orders";
+    public static final String PRODUCT = API_PATH + "/products";
     public static final String REGISTRATION = API_PATH + "/register";
 
     public static final String AUTH = API_PATH + "/auth";
