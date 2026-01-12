7e0123d0853f0152ff949432de9734a0cd76ce2c
Igor Bazhev <bazhew@mail.ru>
2026-01-12 16:48:12 +0500

    Change index controller a bit

--- src/main/java/io/github/onlineshop/web/IndexController.java
+++ src/main/java/io/github/onlineshop/web/IndexController.java
@@ -2,17 +2,17 @@ package io.github.onlineshop.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
     private static final Logger log =
         LoggerFactory.getLogger(IndexController.class);
 
-    @GetMapping("/")
+    @GetMapping("")
     public String getIndex() {
         log.info("Called method getIndex");
-        return "index";
+        return "Index page";
     }
 }
