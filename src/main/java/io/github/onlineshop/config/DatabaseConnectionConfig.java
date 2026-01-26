fea39163c8e3dfee503191dc9705b561bf9d1c19
Igor Bazhev <bazhew@mail.ru>
2026-01-26 16:46:08 +0500

    Update database config

--- src/main/java/io/github/onlineshop/config/DatabaseConnectionConfig.java
+++ src/main/java/io/github/onlineshop/config/DatabaseConnectionConfig.java
@@ -1,35 +1,47 @@
 package io.github.onlineshop.config;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableConfigurationProperties(DatabaseConfig.class)
+@RequiredArgsConstructor
 public class DatabaseConnectionConfig {
     private static final Logger log =
         LoggerFactory.getLogger(DatabaseConnectionConfig.class);
-    private final DatabaseConfig databaseConfig;
 
-    public DatabaseConnectionConfig(DatabaseConfig databaseConfig) {
-        this.databaseConfig = databaseConfig;
-    }
+    private final DatabaseConfig databaseConfig;
 
     @Profile("dev")
     @Bean
-    public String devDatabaseConnection() {
+    public DataSource devDatabaseConnection() {
         log.info("Connected to Dev Database: {}", databaseConfig.getUrl());
-        return "DB Connection for Dev";
+
+        return buildDataSourceFromConfig();
     }
 
     @Profile("prod")
     @Bean
-    public String prodDatabaseConnection() {
+    public DataSource prodDatabaseConnection() {
         log.info("Connected to Prod Database: {}", databaseConfig.getUrl());
-        return "DB Connection for Prod";
+
+        return buildDataSourceFromConfig();
+    }
+
+    private DataSource buildDataSourceFromConfig() {
+        return DataSourceBuilder.create()
+            .url(databaseConfig.getUrl())
+            .username(databaseConfig.getUsername())
+            .password(databaseConfig.getPassword())
+            .build();
     }
 }
